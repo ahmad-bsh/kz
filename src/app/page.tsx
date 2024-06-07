e42e7a31382d654d45c298189771f5c398039088
AhmadBShaik <ahmad.shaik1106@gmail.com>
2024-06-07 16:09:22 +0530

    update home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { siteConfig } from "../../config/site";
 
 export default function Home() {
+  const author = siteConfig.authors.find(
+    (author) => author.name === "AhmadBShaik"
+  );
   return (
-    <main className="flex flex-1 items-center justify-center px-4">
-      <div className="flex flex-col md:items-center">
-        <div className="text-2xl md:text-4xl">
-          {"Welcome to "}
-          <span className="font-bold text-green-500">KodeZnippets</span>!
-        </div>
-        <div className="text-sm md:text-base">
-          Join the journey towards software development mastery.
+    <main className="flex flex-col flex-1 justify-center px-4 max-w-7xl mx-auto w-full">
+      <div className="flex flex-col">
+        <div className="text-sm md:text-base space-y-2.5">
+          <div className="text-2xl md:text-4xl">
+            {"Welcome to "}
+            <span className="font-bold text-green-500">KodeZnippets</span>!
+          </div>
+          <p>
+            Hi! I'm{" "}
+            <Link href={author?.links?.personalSite!}>
+              <span className="text-green-500 font-bold">Ahmad</span>
+            </Link>
+            , a front-end developer specializing in React and Next.js.
+          </p>
+          <p>
+            Welcome to KodeZnippets, where I share my learnings and insights on
+            front-end development.
+          </p>
         </div>
         <Link href="/blog">
           <Button className="mt-10 bg-green-500 hover:bg-green-600 hover:font-bold">
